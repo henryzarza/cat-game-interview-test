49d4ff47d5bf85fe434432cdbaed063885b1f924
Henry Zarza <henryacz7@gmail.com>
2022-06-05 18:27:59 -0500

    Transform data

---
+++ src/data/utils.ts
@@ -0,0 +1,26 @@
+import { CardProps } from '../components/Card';
+
+import { Player } from "./types";
+
+export const transformPlayerData = (data: Player[]) : CardProps[] => {
+  const response = data.map(({ fullName, avatar, games }) => {
+    const oponents = [];
+    let winGames = 0;
+    games.forEach(game => {
+      oponents.push(...game.otherPlayers);
+      if (game.winnerName === fullName) {
+        winGames += 1;
+      }
+    });
+
+    return {
+      fullName,
+      avatar,
+      lastActivityDate: new Date(Math.max.apply(this, games.map(game => new Date(game.accurateDate ? game.accurateDate.date : game.date)))),
+      uniqueOponents: new Set(oponents).size,
+      rate: +(winGames / games.length).toFixed(2)
+    };
+  }).sort((a, b) => b.lastActivityDate.getTime() - a.lastActivityDate.getTime());
+
+  return response;
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,40 @@
+import { useEffect, useState } from 'react';
+
 import { useFakeQuery } from '../data/fakeFetchClient';
 import Message from '../components/Message';
 import Searcher from '../components/Searcher';
 import Loader from '../components/Loader';
-import Card from '../components/Card';
+import Card, { CardProps } from '../components/Card';
+import { transformPlayerData } from '../data/utils';
 
 function Index() {
+  const [players, setPlayers] = useState<CardProps[] | null>(null);
   const { data, error, loading } = useFakeQuery('SelectCatPlayers', {
-    variables: { search: null },
+    variables: { search: null }
   });
 
-  console.log(data);
+  useEffect(() => {
+    if (data && !loading) {
+      setPlayers(transformPlayerData(data));
+    }
+  }, [data, loading]);
 
   if (error) {
-    return <Message />;
+    return <Message message={error.message} isError />;
   }
 
   return (
     <main className='main-content'>
       <h1 className='title main-title'>Recent Activity</h1>
+      <Searcher />
       {loading ? <Loader /> : (
-        <>
-          <Searcher />
+        players?.length ? (
           <section className='card-container'>
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+            {players.map(player => <Card key={player.fullName} data={player} />)}
           </section>
-        </>
+        ) : (
+          <Message message='There are not data' />
+        )
       )}
     </main>
   );
