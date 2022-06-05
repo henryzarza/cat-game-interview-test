f544eeedd102c0f270d743b2e18c27f12ab79a9d
Henry Zarza <henryacz7@gmail.com>
2022-06-05 18:27:49 -0500

    Update Card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,16 +2,26 @@ import Badge from '../Badge';
 
 import styles from './styles.module.css';
 
-function Card() {
+export interface CardProps {
+  lastActivityDate: Date;
+  fullName: string;
+  avatar: string;
+  uniqueOponents: number;
+  rate: number;
+}
+
+function Card({ data } : { data : CardProps }) {
+  const { lastActivityDate, fullName, avatar, uniqueOponents, rate } = data;
+
   return (
     <div className={styles.card}>
-      <img src="https://th-thumbnailer.cdn-si-edu.com/bZAar59Bdm95b057iESytYmmAjI=/1400x1050/filters:focal(594x274:595x275)/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer/95/db/95db799b-fddf-4fde-91f3-77024442b92d/egypt_kitty_social.jpg" alt="Cat" className={styles['card-image']} />
+      <img src={avatar} alt={fullName} className={styles['card-image']} />
       <div className={styles['card-content']}>
-        <h3 className={`small-title ${styles['text-margin']}`}>Alivia Gaines</h3>
-        <span className={`base-text ${styles['text-margin']}`}>Last Activity: 2020-10-20</span>
-        <span className='base-text'>Unique Opponents: 5</span>
+        <h3 className={`small-title ${styles['text-margin']}`}>{fullName}</h3>
+        <span className={`base-text ${styles['text-margin']}`}>Last Activity: {lastActivityDate.toISOString().split('T')[0]}</span>
+        <span className='base-text'>Unique Opponents: {uniqueOponents}</span>
       </div>
-      <Badge className={styles.badge} rate={0.8} />
+      <Badge className={styles.badge} rate={rate} />
     </div>
   );
 }
